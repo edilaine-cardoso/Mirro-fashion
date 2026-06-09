02f5ec6a23529bfc63d9687e786ae04ba7d72ddb
Edilaine Cardoso <edilaine.cardoso.dev@gmail.com>
2026-06-09 13:55:59 +0100

    Delete forma-aoIEFP-main/forma-aoIEFP-main directory

--- forma-aoIEFP-main/forma-aoIEFP-main/Models/Product_Prop.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LearnBlazor.Models
-{
-    public class Product_Prop
-    {
-        public int Id { get; set; }
-        public required string Key { get; set; }
-        public required string Value { get; set; }
-    }
-}
